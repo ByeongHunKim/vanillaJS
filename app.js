6dde2cc90b1a97bf99a1c12a3af6c3d6a117c6a8
ByeongHunKim <rkwhr912@naver.com>
2022-04-14 20:25:58 +0900

    #4.2~4.3 Events part 1,2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
+const link = document.querySelector('a');
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+function handleLinkClicked(event) {
+  event.preventDefault();
+  console.log(event);
+  alert('clicked!');
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
+link.addEventListener('click', handleLinkClicked);
